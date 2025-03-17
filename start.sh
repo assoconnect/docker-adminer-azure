8ecec2e42f22bec37d38f22bcb87de2c272395a2
Mathieu Hamonic <mathieu.hamonic@assoconnect.com>
2025-03-17 20:42:19 +0100

    Make PROTECTED_ACCESS environment variable usable at runtime instead of build

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Setup the basic auth
+echo "AuthType Basic" > /var/www/html/.htaccess &&
+echo "AuthName \"Restricted Access\"" >> /var/www/html/.htaccess &&
+echo "AuthUserFile /var/www/html/.htpasswd" >> /var/www/html/.htaccess &&
+echo "Require valid-user" >> /var/www/html/.htaccess &&
+echo "$PROTECTED_ACCESS" > /var/www/html/.htpasswd
+
 # Start Nginx in the background
 nginx &
 
